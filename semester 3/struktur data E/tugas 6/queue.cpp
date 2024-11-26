c6b790b44b68589a2997b9ec32e3df541a463040
CATSS <85265087+CATSSRIN@users.noreply.github.com>
2024-11-26 18:57:49 +0700

    Update queue.cpp
    added int main, libraries, and where the user can input

--- semester 3/struktur data E/tugas 6/queue.cpp
+++ semester 3/struktur data E/tugas 6/queue.cpp
@@ -1,1 +1,58 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+struct node {
+  int data; 
+    struct node *next;
+};
+typedef struct node node;
+
+void tambahawal(node **head);
+void tambahdata(node **head);
+void transverse(node *head);
+
+int main()
+{
+    node *head;
+    char pilih;
+    do{
+      system("cls");
+      printf("masukkan pilihan\n");
+      printf("1. tambah data di awal\n");
+      printf("2. tambah data di tengah list\n");
+      printf("4. cetak isi list\n");
+      printf("masukkan pilihan (ketik q untuk keluar : )\n");
+      fflush(stdin);
+
+      scanf ("%c", &pilih);
+      if (pilih == "1")
+        tambahawal(&head);
+
+      else if (tambahdata == "2")
+          tambahdata(&head);
+        
+      else if (pilih == "4")
+        transverse(head);
+        getch();
+    }
+} while (pilih != "q");
+
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
