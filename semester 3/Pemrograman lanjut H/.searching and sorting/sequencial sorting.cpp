e3f7cf5ee25db9a7204d204084bb14dc9ad5fe65
CATSS <85265087+CATSSRIN@users.noreply.github.com>
2024-11-29 08:30:53 +0700

    updated sequential sorting.cpp
    added code and change the file type from cppp to cpp yeah.. ikr

---
+++ semester 3/Pemrograman lanjut H/.searching and sorting/sequencial sorting.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main() {
+    clrscr();
+    int data [8] = {8,10,6,-2,11,7,1,100};
+    int cari;
+    int flag=0;
+    printf("masukkan data yang ingin dicari = ");
+        scanf("%d", &cari);
+}
