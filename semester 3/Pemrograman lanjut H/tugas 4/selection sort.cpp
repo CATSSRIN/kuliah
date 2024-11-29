992d085860c64198cd2abb510e6472448f7328a7
CATSS <85265087+CATSSRIN@users.noreply.github.com>
2024-11-29 19:55:27 +0700

    Update selection sort.cpp

--- semester 3/Pemrograman lanjut H/tugas 4/selection sort.cpp
+++ semester 3/Pemrograman lanjut H/tugas 4/selection sort.cpp
@@ -9,7 +9,7 @@ void selectionSort(int arr[], int n) {
                 min_idx = j;
             }
         }
-        // Swap the found minimum element with the first element
+        
         temp = arr[min_idx];
         arr[min_idx] = arr[i];
         arr[i] = temp;
