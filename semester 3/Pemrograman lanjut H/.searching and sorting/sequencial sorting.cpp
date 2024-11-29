131a6bcc711c8cc46f2584168622d9407002cd2d
CATSS <85265087+CATSSRIN@users.noreply.github.com>
2024-11-29 08:39:17 +0700

    Update sequencial sorting.cpp

--- semester 3/Pemrograman lanjut H/.searching and sorting/sequencial sorting.cpp
+++ semester 3/Pemrograman lanjut H/.searching and sorting/sequencial sorting.cpp
@@ -8,4 +8,12 @@ void main() {
     int flag=0;
     printf("masukkan data yang ingin dicari = ");
         scanf("%d", &cari);
+    for (int i=0; i<8; i++) {
+        if (data[i] == cari)
+            flag=1;
+    }
+    if (flag==1)
+        printf("Data ditemukan");
+    else
+        printf("Data tidak ditemukan");
 }
