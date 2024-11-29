f940afc744b50bc608faadefaf1708848c6d9494
CATSS <85265087+CATSSRIN@users.noreply.github.com>
2024-11-29 19:54:24 +0700

    Update quick sort.cpp

--- semester 3/Pemrograman lanjut H/tugas 4/quick sort.cpp
+++ semester 3/Pemrograman lanjut H/tugas 4/quick sort.cpp
@@ -1,22 +1,19 @@
 #include <iostream>
 using namespace std;
 
-// Function to swap two elements
 void swap(int* a, int* b) {
     int t = *a;
     *a = *b;
     *b = t;
 }
 
-// Partition function
 int partition(int arr[], int low, int high) {
-    int pivot = arr[high]; // pivot
-    int i = (low - 1); // Index of smaller element
+    int pivot = arr[high]; 
+    int i = (low - 1); 
 
     for (int j = low; j <= high - 1; j++) {
-        // If current element is smaller than or equal to pivot
         if (arr[j] <= pivot) {
-            i++; // increment index of smaller element
+            i++; 
             swap(&arr[i], &arr[j]);
         }
     }
@@ -24,26 +21,22 @@ int partition(int arr[], int low, int high) {
     return (i + 1);
 }
 
-// QuickSort function
 void quickSort(int arr[], int low, int high) {
     if (low < high) {
-        // pi is partitioning index, arr[p] is now at right place
+
         int pi = partition(arr, low, high);
 
-        // Separately sort elements before partition and after partition
         quickSort(arr, low, pi - 1);
         quickSort(arr, pi + 1, high);
     }
 }
 
-// Function to print an array
 void printArray(int arr[], int size) {
     for (int i = 0; i < size; i++)
         cout << arr[i] << " ";
     cout << endl;
 }
 
-// Main function to test the QuickSort algorithm
 int main() {
     int arr[] = {10, 7, 8, 9, 1, 5};
     int n = sizeof(arr) / sizeof(arr[0]);
