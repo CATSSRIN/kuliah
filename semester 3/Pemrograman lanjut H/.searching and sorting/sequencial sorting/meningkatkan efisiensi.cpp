b4ac34b9426858ec4a6cc7e89027423537be8464
CATSS <85265087+CATSSRIN@users.noreply.github.com>
2024-11-29 09:14:00 +0700

    Update meningkatkan efisiensi.cpp

---
+++ semester 3/Pemrograman lanjut H/.searching and sorting/sequencial sorting/meningkatkan efisiensi.cpp
@@ -0,0 +1,27 @@
+#include <conio.h>
+#include <stdlib.h>
+
+void main() {
+    clrscr();
+    int data[100];
+    int cari=20;
+    int counter=0;
+    int flag=0;
+    randomize();
+    printf (int i=0; i<100; i++) {
+        data[i]=random(100);
+        printf ("%d ", data[i]);
+    }
+    printf("\ndone. \n");
+
+    for (int i=0; i<100; i++) {
+        if (data[i]==cari) {
+            flag=1;
+            counter++;
+        }
+    }
+    if (flag==1)
+        printf("Data ditemukan sebanyak: %d\n", counter);
+    else
+        printf("Data tidak ditemukan");
+}
