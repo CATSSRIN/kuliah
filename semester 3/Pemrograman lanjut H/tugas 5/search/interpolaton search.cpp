06fdd5f7103341a42d31ec815a43f87b358275c8
CATSS <85265087+CATSSRIN@users.noreply.github.com>
2024-12-07 22:43:24 +0700

    fixed some spaces

--- semester 3/Pemrograman lanjut H/tugas 5/search/interpolaton search.cpp
+++ semester 3/Pemrograman lanjut H/tugas 5/search/interpolaton search.cpp
@@ -6,23 +6,22 @@
 void main(){
     clrscr ();
     int data[9] = {3, 9, 11, 12, 15, 17, 23, 31, 35};
-    int low;high;
+    int low, high;
     int flag=0, d=36;
     float pos1;
     int pos, n=9;
     low=0;
     high=n-1;
-    do(
+    do {
         pos1 = (float) (d-data[low]) / (data[high]-data[low]) * (high-low) + low;
-        pos = floor
-        if (data[pos] == d) (
+        pos = floor(pos1);
+        if (data[pos] == d) {
             flag=1;
             break;
-        )
-        if (data[pos] > d) high = pos = pos-1;
-        else
-            if (data[pos] < d) low = pos = pos+1;
-    ) 
+        }
+        if (data[pos] > d) high = pos - 1;
+        else if (data[pos] < d) low = pos + 1;
+    } 
     while (d >= data[low] && d <= data[high]);
     if (flag==0) printf("Data tidak ditemukan\n");
     else printf("Data ditemukan pada indeks ke-%d\n", pos);
