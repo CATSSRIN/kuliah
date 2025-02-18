6a5164761f3eff3c67846a456e2a87bdc03f9906
CATSS <85265087+CATSSRIN@users.noreply.github.com>
2025-02-18 13:01:35 +0700

    Update oop.cpp

--- semester 4/PBO B/week 2/oop.cpp
+++ semester 4/PBO B/week 2/oop.cpp
@@ -4,8 +4,16 @@ using namespace std;
 
 class cls{
     private:
+        string nama; 
     protected:
     public:
+        string getNama(){
+            return this->nama;
+        }
+
+        void setNama(string nama){
+            this->nama = nama;
+        }
 };
 
 int main(){
