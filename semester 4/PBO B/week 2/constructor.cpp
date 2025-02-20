f019e31db14f9d5637c0c782f4d3e1c3203d8e49
CATSS <85265087+CATSSRIN@users.noreply.github.com>
2025-02-20 16:50:17 +0700

    Create constructor.cpp

---
+++ semester 4/PBO B/week 2/constructor.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+class student{
+    public:
+        std::string name;
+        int age;
+        double gpa;
+
+student (std::string name, int age, double gpa){
+    this->name = name;
+    this->age = age;
+    this->gpa = gpa;
+
+    }
+
+}; 
+
+int main(){
+
+    student student1("spongebob", 25, 3.2);
+
+    std::cout << student1.name << '\n';
+
+    return 0;
+}
+
+
+
+int main(){
+
+    student student1("spongebob", 25, 3.2);
+
+    std::cout << student1.name << '\n';
+
+    return 0;
+}
