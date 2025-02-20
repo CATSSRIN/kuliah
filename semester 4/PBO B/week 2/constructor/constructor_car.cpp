246b73fcc78926866f8e0161a2428cda5aa2c90f
CATSS <85265087+CATSSRIN@users.noreply.github.com>
2025-02-20 17:10:44 +0700

    added some variation

--- semester 4/PBO B/week 2/constructor/constructor_car.cpp
+++ semester 4/PBO B/week 2/constructor/constructor_car.cpp
@@ -19,10 +19,21 @@ class car{
 int main() {
 
     car car1("Toyota", "Avanza", 2019, "Black");
+    car car2("ford", "mustang", 2020, "red");
+
 
     std::cout << car1.make << '\n';
     std::cout << car1.model << '\n';
     std::cout << car1.year << '\n';
     std::cout << car1.color << '\n';
+    std::cout << '\n';
+
+    printf(car2.make.c_str());
+    printf("\n");
+    printf(car2.model.c_str());
+    printf("\n");
+    printf("%d\n", car2.year);
+    printf(car2.color.c_str());
+    printf("\n");
 
 };
