61fcd4b97f67bf7deff2c94d656ea3c9037661dc
CATSS <85265087+CATSSRIN@users.noreply.github.com>
2025-02-20 17:04:05 +0700

    Create constructor_car.cpp

---
+++ semester 4/PBO B/week 2/constructor/constructor_car.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+class car{
+    public:
+        std::string make;
+        std::string model;
+        int year;
+        std::string color;
+
+    car(std::string make, std::string model, int year, std::string color){
+        this->make = make;
+        this->model = model;
+        this->year = year;
+        this->color = color;
+    }
+
+};
+
+int main() {
+
+
+
+};
