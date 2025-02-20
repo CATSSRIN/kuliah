3ec86967018c59f7ebf43ab26daa21f0f11ffc51
CATSS <85265087+CATSSRIN@users.noreply.github.com>
2025-02-20 16:57:47 +0700

    added formatting

--- semester 4/PBO B/week 2/constructor/constructor.cpp
+++ semester 4/PBO B/week 2/constructor/constructor.cpp
@@ -18,10 +18,23 @@ student (std::string name, int age, double gpa){
 int main(){
 
     student student1("spongebob", 25, 3.2);
+    student student2("patrick", 40, 1.5);
+    student student3("sandy", 21, 4.0);
 
     std::cout << student1.name << '\n';
     std::cout << student1.age << '\n';
     std::cout << student1.gpa << '\n';
+    printf("\n");
+
+    std::cout << student2.name << '\n';
+    std::cout << student2.age << '\n';
+    std::cout << student2.gpa << '\n';
+    printf("\n");
+
+    std::cout << student3.name << '\n';
+    std::cout << student3.age << '\n';
+    std::cout << student3.gpa << '\n';
+    printf("\n");
 
     return 0;
 };
