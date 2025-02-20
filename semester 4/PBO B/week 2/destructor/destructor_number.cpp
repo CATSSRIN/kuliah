2953b528308d6c8243311251720886fdaee7b7a1
CATSS <85265087+CATSSRIN@users.noreply.github.com>
2025-02-20 17:32:20 +0700

    Create destructor_number.cpp

---
+++ semester 4/PBO B/week 2/destructor/destructor_number.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+class number{
+    private:
+        double num;
+
+    public:
+        number(double num)
+        {
+            this->num = num;
+            cout << "constuctor is being called" << endl;
+            cout << "number is " << num << endl;
+        }
+
+            ~number(){
+            cout<<"Object is being deleted by destructor"<<endl;
+            cout << "number is " << num << endl;
+        }
+
+
+        int main(){
+            number seven(7);
+            number five(5);
+            number six(6);
+
+            return 0;
+        }
+};
