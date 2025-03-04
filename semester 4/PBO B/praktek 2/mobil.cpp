4fcea8980d17c84b9575363445dc998f7d7788bb
CATSS <85265087+CATSSRIN@users.noreply.github.com>
2025-03-04 19:04:08 +0700

    Create mobil.cpp

---
+++ semester 4/PBO B/praktek 2/mobil.cpp
@@ -0,0 +1,22 @@
+#include "iostream"
+
+using namespapce std;
+class mobil{
+    public:
+        //data member
+        string merk, model, warna, nomorplat, nomorrangka, nomormesin;
+
+    //member function
+    void maju(){
+
+    }
+
+    void mundur(){
+
+    }
+
+    void belok(){
+        
+    }
+
+}
