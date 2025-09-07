f0cdbbb89a36e7e83d34ae407c3529e6ca396c7e
CATSS <85265087+CATSSRIN@users.noreply.github.com>
2025-09-07 21:47:20 +0700

    Update Tugas1Framework.php

--- semester 5/Pemrograman framework A/Tugas 1/Tugas1Framework.php
+++ semester 5/Pemrograman framework A/Tugas 1/Tugas1Framework.php
@@ -28,17 +28,16 @@ if (isset($_GET['page'])) {
 <?php
 if ($page == 'home') {
     echo "<h2>Selamat Datang di home</h2>";
+    echo "<body> selamat datang di landing page / home </body><br>";
 } elseif ($page == 'about') {
     echo "<h2>About Us</h2>";
+    echo "<body> ini adalah halaman about </body><br>";
 } elseif ($page == 'profile') {
-    echo "<h2>Nama, NPM, no Wa</h2>";
-    echo "<body> Deny Yusuf Marcheno, 23081010154, 081333194794 </body><br>"; 
-    echo "<body> Caezarlov Nugraha, 23081010182, 08170040805 </body><br>";
-    echo "<body> Fauzan Indarwan Zacky, 23081010222, 082232947171 </body><br>";
-    echo "<body> Vox Dei Purba, 23081010184, 082162122434 </body><br>";
-    echo "<body> Farrel Zikri Suryahadi, 23081010213, 085746007677 </body><br>";
+    echo "<h2>Nama || NPM || no Wa</h2>";
+    echo "<body> | Caezarlov Nugraha || 23081010182 || 08170040805 | </body><br>";
 } elseif ($page == 'contact') {
     echo "<h2>Contact Us</h2>";
+    echo "<body> Hubungi saya di Lappy@waifu.club </body><br>";
 } else {
     echo "<h2>404 Page Not Found</h2>";
 }
