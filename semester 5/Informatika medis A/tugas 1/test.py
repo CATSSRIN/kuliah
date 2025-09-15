6744a480dd3e585bb43013af27d8610d29b36d85
CATSS <85265087+CATSSRIN@users.noreply.github.com>
2025-09-15 12:41:21 +0700

    testing cv2

---
+++ semester 5/Informatika medis A/tugas 1/test.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('xray.png', 0)
+
+# Histogram equalization
+he = cv2.equalizeHist(img)
+
+# CLAHE
+clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+cl = clahe.apply(img)
+
+# Gamma correction
+gamma = 1.5
+gc = np.array(255*(img/255)**gamma, dtype='uint8')
+
+# Filtering
+med = cv2.medianBlur(img, 3)
+gauss = cv2.GaussianBlur(img, (5,5), 0)
+
